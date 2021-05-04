de1814ed7c9d164c089c19916955628df6eba02e
Ian Sokolskyi <iansokolskyi@gmail.com>
2021-05-04 16:38:18 +0300

    remove Dockerfile, .dockerignore, set default port

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(process.env.MESSAGE);
+  res.write(
+    JSON.stringify({message: 'new message', fromEnv: process.env.MESSAGE})
+  )
+  res.end();
 });
 
 server.listen(port, hostname, () => {
